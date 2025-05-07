bf9f5b8088b2f6740864ea43a39677b8aec1c44a
Leo Caldosa <caldosaleo@gmail.com>
2025-05-08 01:42:53 +0800

    import tkinter, setup gui window & frames, mainloop, main guard, and corrected end_screen pseudocode

--- quiz_player.py
+++ quiz_player.py
@@ -1,4 +1,5 @@
 # import tkinter
+import tkinter as tk
 
 # define get_boolean function
 
@@ -19,6 +20,7 @@
 # define show_correct_answer_screen function
 
 # define main function
+def main():
     # open file for reading
         # iterate over lines 0 to 6
             # save topic of quiz
@@ -32,10 +34,14 @@
         # call store_questions function
 
     # set up root window
+    root = tk.Tk()
 
     # set question_screen as tkframe
+    question_screen = tk.Frame()
     # set show_correct_answer_screen as tkframe
+    show_correct_answer_screen = tk.Frame()
     # set end_screen as tkframe
+    end_screen = tk.Frame()
 
     # iterate over the keys in questions dictionary (this will be made in store_questions function)
         # iterate over number of tries
@@ -48,10 +54,13 @@
             # call show_correct_answer_screen function
 
     # check if settings[show_score_end]
-        # put score as tk.label
+        # put score as tk.label on end screen
     # check if correct_answers (this dictionary will be made in store_questions function)
-        # put correct_answers as tk.label
+        # put correct_answers as tk.label on end screen
 
     # run tk.mainloop()
+    tk.mainloop()
 
 # create main function guard
+if __name__ == "__main__":
+    main()
